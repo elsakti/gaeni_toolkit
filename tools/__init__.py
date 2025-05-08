9884e7a2d6d9abe1a2be433f56e61eda9af7a3a4
elsakti <svkakazone@gmail.com>
2025-05-08 15:41:29 +0700

    fix

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,37 +1,38 @@
 import os
 from langchain.agents import Tool
 from newsapi import NewsApiClient
-from langchain_community.utilities import GoogleSerperAPIWrapper, OpenWeatherMapAPIWrapper
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+from crewai_tools import SerperDevTool
 
-def image_searching(query, serper_key):
-    try:
-        image_search_tool = GoogleSerperAPIWrapper(api_key=serper_key, type="images")
-        results = image_search_tool.results(query)
+# def image_searching(query, serper_key):
+#     try:
+#         image_search_tool = GoogleSerperAPIWrapper(api_key=serper_key, type="images")
+#         results = image_search_tool.results(query)
         
-        if results and len(results) > 0:
-            first_result = results[0]
-            image_url = first_result.get("image_url")
-            description = first_result.get("description", "No description available.")
-            return image_url, description
-        return None, None
-    except Exception as e:
-        print(f"Error fetching image: {str(e)}")
-        return None, None
+#         if results and len(results) > 0:
+#             first_result = results[0]
+#             image_url = first_result.get("image_url")
+#             description = first_result.get("description", "No description available.")
+#             return image_url, description
+#         return None, None
+#     except Exception as e:
+#         print(f"Error fetching image: {str(e)}")
+#         return None, None
 
 class Tools:
     def search_tool(self, serper_key):
         return Tool(
             name="Search",
-            func=GoogleSerperAPIWrapper(api_key=serper_key).run,
+            func=SerperDevTool(api_key=serper_key).run,
             description="A powerful search tool for finding up-to-date information on various topics. Use this to answer questions about current events, facts, or general knowledge. Input should be a specific, well-formulated question or search query. Returns relevant search results including snippets and URLs."
         )
          
-    def image_search(self, serper_key):
-        return Tool(
-            name="Image Search",
-            func=image_searching(serper_key=serper_key),
-            description="Useful for when you need to find images of something. Input should be a search query."
-        )
+    # def image_search(self, serper_key):
+    #     return Tool(
+    #         name="Image Search",
+    #         func=image_searching(serper_key=serper_key),
+    #         description="Useful for when you need to find images of something. Input should be a search query."
+    #     )
     
     def weather_tool(self, weather_key):
         os.environ["OPENWEATHERMAP_API_KEY"] = weather_key
